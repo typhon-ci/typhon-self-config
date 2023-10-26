4d70fe874c45b8ec76c9a853d286ffbe699d8b24
Paul-Nicolas Madelaine <pnm@pnm.tf>
2023-10-26 15:48:27 +0200

    add flake schemas

--- flake.nix
+++ flake.nix
@@ -1,6 +1,14 @@
 {
-  inputs.typhon.url = "github:typhon-ci/typhon";
-  outputs = { self, typhon }: {
+  inputs = {
+    typhon.url = "github:typhon-ci/typhon";
+    flake-schemas.follows = "typhon/flake-schemas";
+  };
+
+  outputs = {
+    self,
+    typhon,
+    flake-schemas,
+  }: {
     typhonProject = typhon.lib.github.mkGithubProject {
       title = "Typhon";
       description = "Nix-based continuous integration";
@@ -8,6 +16,10 @@
       repo = "typhon";
       secrets = ./secrets.age;
       typhon_url = "https://etna.typhon-ci.org";
+    };
+    schemas = {
+      inherit (typhon.lib.schemas) typhonProject;
+      inherit (flake-schemas.schemas) schemas;
     };
   };
 }
