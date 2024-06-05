6fc731b4b2bd771b87a71a912ee3cac037cc8d52
Paul-Nicolas Madelaine <pnm@pnm.tf>
2024-06-05 16:53:45 +0200

    format

--- flake.nix
+++ flake.nix
@@ -4,37 +4,39 @@
     flake-schemas.follows = "typhon/flake-schemas";
   };
 
-  outputs = {
-    self,
-    typhon,
-    flake-schemas,
-  }: {
-    typhonProject = typhon.lib.github.mkProject {
-      title = "Typhon";
-      description = "Nix-based continuous integration";
-      owner = "typhon-ci";
-      repo = "typhon";
-      secrets = ./secrets.age;
-      typhonUrl = "https://etna.typhon-ci.org";
-      deploy = [
-        {
-          name = "Push to Cachix";
-          value = typhon.lib.cachix.mkPush {name = "typhon";};
-        }
-        {
-          name = "Deploy the documentation";
-          value = typhon.lib.github.mkPages {
-            owner = "typhon-ci";
-            repo = "typhon";
-            job = "doc";
-            customDomain = "doc.typhon-ci.org";
-          };
-        }
-      ];
+  outputs =
+    {
+      self,
+      typhon,
+      flake-schemas,
+    }:
+    {
+      typhonProject = typhon.lib.github.mkProject {
+        title = "Typhon";
+        description = "Nix-based continuous integration";
+        owner = "typhon-ci";
+        repo = "typhon";
+        secrets = ./secrets.age;
+        typhonUrl = "https://etna.typhon-ci.org";
+        deploy = [
+          {
+            name = "Push to Cachix";
+            value = typhon.lib.cachix.mkPush { name = "typhon"; };
+          }
+          {
+            name = "Deploy the documentation";
+            value = typhon.lib.github.mkPages {
+              owner = "typhon-ci";
+              repo = "typhon";
+              job = "doc";
+              customDomain = "doc.typhon-ci.org";
+            };
+          }
+        ];
+      };
+      schemas = {
+        inherit (typhon.lib.schemas) typhonProject;
+        inherit (flake-schemas.schemas) schemas;
+      };
     };
-    schemas = {
-      inherit (typhon.lib.schemas) typhonProject;
-      inherit (flake-schemas.schemas) schemas;
-    };
-  };
 }
